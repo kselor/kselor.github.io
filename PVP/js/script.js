86e68c88aa7787a39805de4a616483f40507be8f
kselor <62292636+kselor@users.noreply.github.com>
2020-09-03 22:01:24 +0500

    Add files via upload

---
+++ PVP/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    
+    $('.news_items').slick({
+        nextArrow: $('.news_arrow_next'),
+        prevArrow: $('.news_arrow_prev'),
+        appendDots: $('.news_dots'),
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+      });
+
+});
