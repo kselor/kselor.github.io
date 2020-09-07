7242261954d994ee52005c5b3f37f0078ab23ddc
kselor <62292636+kselor@users.noreply.github.com>
2020-09-07 09:51:15 +0500

    Add files via upload

---
+++ PVP/js/calculator.js
@@ -0,0 +1,149 @@
+$(document).ready(function () {
+
+    let optionCount = $('.calculator_option_count'),
+        optionCar = $('.calculator_item:eq(4) .calculator_option'),
+        optionWeight = $('.calculator_item:eq(5) .calculator_option'),
+        optionTrailer = $('.calculator_item:eq(6) .calculator_option'),
+        priceWeight = 0,
+        priceTrailer = 0,
+        totalCount = 1,
+        totalPrice = 0;
+
+
+    optionCar.each(function (i) {
+
+        $(this).on('click', function () {
+
+            $(this).addClass('active').siblings().removeClass('active');
+
+            if (i == 0) {
+                optionWeight.detach().removeClass('active');
+                optionWeight.eq(0).appendTo($('.calculator_item:eq(5)')).html('Нет ограничений').val('0').trigger('click');
+                optionTrailer.detach().removeClass('active');
+                optionTrailer.eq(1).appendTo($('.calculator_item:eq(6)')).trigger('click');
+                optionCount.css('display', 'none');
+                $('.calculator_order_title:eq(0)').find('span').html('M1')
+                $('.calculator_order_icon:eq(0)').html('<img src="img/icons/calculator/calc_order_M1.png" alt="">')
+                $('.calculator_order_title:eq(1)').find('span').html('O1, O2')
+                $('.calculator_order_item:eq(1)').css('display', 'flex')
+                $('.calculator_order_descr').html('Транспортные средства, используемые для перевозки пассажиров и имеющие, помимо места водителя, не более восьми мест для сидения.')
+            } else {
+                optionTrailer.detach().removeClass('active');
+                optionWeight.appendTo($('.calculator_item:eq(5)')).eq(0).html('до 3.5 тонн').val('572').trigger('click');
+                optionTrailer.eq(0).appendTo($('.calculator_item:eq(6)')).trigger('click');
+                optionCount.css('display', 'flex');
+                $('.calculator_order_descr').html('Транспортные средства, предназначенные для перевозки грузов.')
+
+            }
+
+        });
+
+    });
+
+    optionWeight.each(function (i) {
+
+        $(this).on('click', function () {
+
+            $(this).addClass('active').siblings().removeClass('active');
+
+            priceWeight = $(this).val();
+            totalPrice = priceWeight + priceTrailer;
+            $('.calculator_order_price').html(totalPrice + ' ₽')
+
+            
+            if (i <= 1) {
+                optionTrailer.detach().removeClass('active');
+                optionTrailer.eq(0).appendTo($('.calculator_item:eq(6)')).trigger('click')
+
+                if (i == 0) {
+                    $('.calculator_order_title:eq(0)').find('span').html('N1')
+                    $('.calculator_order_icon:eq(0)').html('<img src="img/icons/calculator/calc_order_N1.png" alt="">')
+                }
+
+                if (i == 1) {
+                    $('.calculator_order_title:eq(0)').find('span').html('N2')
+                    $('.calculator_order_icon:eq(0)').html('<img src="img/icons/calculator/calc_order_N2.png" alt="">')
+                }
+
+            }
+
+            if (i > 1) {
+                optionTrailer.appendTo($('.calculator_item:eq(6)'));
+                $('.calculator_order_title:eq(0)').find('span').html('N3')
+                $('.calculator_order_icon:eq(0)').html('<img src="img/icons/calculator/calc_order_N3.png" alt="">')
+            }
+
+        });
+
+    });
+
+    let lastClick = 1;
+    optionTrailer.each(function (i) {
+
+
+        $(this).on('click', function () {
+
+            $(this).addClass('active').siblings().removeClass('active');
+
+            if ($('.calculator_option.active')) {
+                optionCount.appendTo($(this));
+            }
+            if (i == 0) {
+                optionCount.detach();
+            }
+
+            if (lastClick != i) {
+                totalCount = 1;
+                $('.option_count_value').html(totalCount);
+            }
+            lastClick = i;
+
+            priceTrailer = $(this).val();
+            totalPrice = priceWeight + priceTrailer * totalCount;
+            $('.calculator_order_price').html(totalPrice + ' ₽');
+
+
+            if (i == 0) {
+                $('.calculator_order_item:eq(1)').css('display', 'none')
+            }
+
+            if (i == 1) {
+                $('.calculator_order_title:eq(1)').find('span').html('O1, O2')
+                $('.calculator_order_item:eq(1)').css('display', 'flex')
+            }
+
+            if(i > 1){
+                $('.calculator_order_title:eq(1)').find('span').html('O3')
+                $('.calculator_order_item:eq(1)').css('display', 'flex')
+            }
+            if(i > 2){
+                $('.calculator_order_title:eq(1)').find('span').html('O4')
+                $('.calculator_order_item:eq(1)').css('display', 'flex')
+            }
+
+            $('.calculator_order_value').html(totalCount + ' шт')
+        });
+    });
+
+    
+
+    $('.calculator_option .plus').on('click', function () {
+        if (totalCount < 3) {
+            totalCount = totalCount + 1;
+        }
+        $('.option_count_value').html(totalCount);
+
+    });
+
+    $('.calculator_option .minus').on('click', function () {
+        if (totalCount > 1) {
+            totalCount = totalCount - 1;
+        }
+        $('.option_count_value').html(totalCount);
+    });
+
+    optionCar.eq(1).trigger('click')
+    optionWeight.eq(0).trigger('click');
+    optionTrailer.eq(0).trigger('click');
+
+});
